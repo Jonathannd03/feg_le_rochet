c6a0f4e8923b4412199adcaabaefa2ca57db1f92
jonathanndinga <ndingajonathan96@gmail.com>
2023-02-11 19:41:57 +0100

    configured memdia processing

---
+++ src/constants/images.js
@@ -0,0 +1,10 @@
+import feg_logo from "../assets/images/feg-logo.svg";
+import hamburger from "../assets/images/hamburger.svg";
+import headerBg from "../assets/images/header-bg.jpg";
+
+// eslint-disable-next-line
+export default {
+  feg_logo,
+  hamburger,
+  headerBg,
+};
